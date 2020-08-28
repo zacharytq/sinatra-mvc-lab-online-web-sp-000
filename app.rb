189a0771e9e34fd20901e151f13b9e82c5112f64
chic-page-6502 <zacharytq@gmail.com>
2020-08-28 15:43:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+
+    word = PigLatinizer.new
+    @pig_latinized_word = word.to_pig_latin(params[:user_phrase])
+
+    erb :pit_latin
+  end
 end
